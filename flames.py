e48745855398fdc2bf7373eb96759c56268fd454
Rgohil18 <ravigohil2006@gmail.com>
2024-07-29 00:01:27 +0530

    employee project

--- flames.py
+++ flames.py
@@ -1,30 +1,5 @@
-x = input("Current currency")
-y = input("Required currency")
-X1 = "USA"
-X2 = "INDIA"
-X3 = "EUROPE"
-Amount1 = float(input())
-if x==X1:
-    if y==X2:
-        Finalamount=Amount1*83.74
-        print(Finalamount)
-if x==X2:
-    if y==X1:
-        Finalamount=Amount1/83.74
-        print(Finalamount)
-if x==X3:
-    if y==X2:
-        Finalamount=Amount1*91.02
-        print(Finalamount)
-if x==X3:
-    if y==X1:
-        Finalamount=Amount1*1.08715
-        print(Finalamount)
-if x==X2:
-    if y==X3:
-        Finalamount=Amount1/91.02
-        print(Finalamount)
-if x==X2:
-    if y==X3:
-        Finalamount=Amount1/91.02
-        print(Finalamount)
+import mysql.connector
+
+
+con = mysql.connector.connect(
+    host="localhost", user="root", password="password", database="emp")
