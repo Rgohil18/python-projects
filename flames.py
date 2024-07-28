aff308ced601896b0ae6a57cc44c10d3416c8fcd
Rgohil18 <ravigohil2006@gmail.com>
2024-07-28 21:09:38 +0530

    Currency converter

--- flames.py
+++ flames.py
@@ -1,5 +1,30 @@
-name1 = "raviraj"
-std = "11th"
-x = 1
-print(f"my name is {name1},i am studying in {std}")
-print(x)
+x = input("Current currency")
+y = input("Required currency")
+X1 = "USA"
+X2 = "INDIA"
+X3 = "EUROPE"
+Amount1 = float(input())
+if x==X1:
+    if y==X2:
+        Finalamount=Amount1*83.74
+        print(Finalamount)
+if x==X2:
+    if y==X1:
+        Finalamount=Amount1/83.74
+        print(Finalamount)
+if x==X3:
+    if y==X2:
+        Finalamount=Amount1*91.02
+        print(Finalamount)
+if x==X3:
+    if y==X1:
+        Finalamount=Amount1*1.08715
+        print(Finalamount)
+if x==X2:
+    if y==X3:
+        Finalamount=Amount1/91.02
+        print(Finalamount)
+if x==X2:
+    if y==X3:
+        Finalamount=Amount1/91.02
+        print(Finalamount)
