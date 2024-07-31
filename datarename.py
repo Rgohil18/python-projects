ec3d62695189114b1881868cf71bee93f8014afd
Rgohil18 <ravigohil2006@gmail.com>
2024-07-31 22:53:46 +0530

    code for changing file name

--- datarename.py
+++ datarename.py
@@ -1,13 +1,19 @@
-import shutil
 import os
-import dataset
+import shutil
+import sys
 
-source1 = open(r"C:\Users\ravig\OneDrive\Desktop\python-projects\dataset\Animals")
-print(source1)
-source2 = list[source1]
-for i in source2:
-    for x in i:
-        list1=[x]
-    if (list1=="jpg."):
-        os.rename(r"C:\Users\ravig\OneDrive\Desktop\python-projects\dataset\Animals",
-                  "C:\Users\ravig\OneDrive\Desktop\python-projects\dataset\Animalsnew")
+data_path = r"C:\Users\ravig\OneDrive\Desktop\python-projects\Animals"
+data_path_list = os.listdir(data_path)
+
+for i in data_path_list:
+    folder_path = os.path.join(data_path,i)
+    folder_path_list = os.listdir(folder_path)
+    for j in folder_path_list:
+        new_name = j.split("_")[1]
+        new_name = i + "_" + new_name
+        old_image_path =  os.path.join(folder_path,j)
+        new_image_path = os.path.join(folder_path,new_name)
+        os.rename(old_image_path,new_image_path)
+
+
+        
