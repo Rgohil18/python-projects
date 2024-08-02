099a85b44b3505b61fd5620257722a03405fef37
Rgohil18 <ravigohil2006@gmail.com>
2024-08-02 22:37:38 +0530

    Practice of class,__init__,

--- flames.py
+++ flames.py
@@ -1,2 +1,19 @@
-import os
-print(os.name)
+class employee:
+    def __init__(self,name,time):
+        self.name = name
+        self.time = time
+    def print(self):
+        print(f"my name is {self.name},i have joined from last {self.time} days")
+class person(employee):
+    def __init__(self,gender):
+        self.gender = gender
+        print(f"my gender is {self.gender}")
+
+
+
+x = employee("ravi",30)
+x.print()
+x = person("ravi")
+y = employee("vijay",45)
+y.print()
+
