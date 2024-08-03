543382b7930e644603252c5ab06c2642d0138513
Rgohil18 <ravigohil2006@gmail.com>
2024-08-03 22:43:24 +0530

    init and inheritance practice

--- flames.py
+++ flames.py
@@ -2,18 +2,31 @@ class employee:
     def __init__(self,name,time):
         self.name = name
         self.time = time
-    def print(self):
-        print(f"my name is {self.name},i have joined from last {self.time} days")
+        print(f"my name is {self.name}, i have joined the company from last {self.time}"
+              )
+    def age(self,age):
+        self.age = age
+        print(f"my age is {self.age}")
+
 class person(employee):
     def __init__(self,gender):
         self.gender = gender
         print(f"my gender is {self.gender}")
 
+class place(person):
+    def __init__(self,place):
+        self.place=place
+        print(f"i live in {self.place}")
 
+def add(a,b):
+        print(a+b)
 
+x = place("mumbai")
+x.age(18)
 x = employee("ravi",30)
-x.print()
-x = person("ravi")
-y = employee("vijay",45)
-y.print()
+x = person("male")
+x = add(1,2)
+
+
+
 
