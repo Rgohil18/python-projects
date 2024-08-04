1fc9cb173a1745153ba07dbe7d36f007fbe22d14
Rgohil18 <ravigohil2006@gmail.com>
2024-08-04 23:57:07 +0530

    decoratos and encapsulation practice

--- flames.py
+++ flames.py
@@ -1,31 +1,15 @@
-class employee:
-    def __init__(self,name,time):
-        self.name = name
-        self.time = time
-        print(f"my name is {self.name}, i have joined the company from last {self.time}"
-              )
-    def age(self,age):
-        self.age = age
-        print(f"my age is {self.age}")
 
-class person(employee):
-    def __init__(self,gender):
-        self.gender = gender
-        print(f"my gender is {self.gender}")
+def greet(fx):
+  def mfx():
+        print("good morning")
+        fx()
+        print("thankyou for using our website")
+        return mfx()
+@greet
+def op():
+    print("hello world")
+op()
 
-class place(person):
-    def __init__(self,place):
-        self.place=place
-        print(f"i live in {self.place}")
-
-def add(a,b):
-        print(a+b)
-
-x = place("mumbai")
-x.age(18)
-x = employee("ravi",30)
-x = person("male")
-x = add(1,2)
 
 
 
