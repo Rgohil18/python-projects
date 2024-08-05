7c20be1c3359c7b4af4fa7d67c3aedb2e97fae28
Rgohil18 <ravigohil2006@gmail.com>
2024-08-05 23:44:24 +0530

    try and except practice

--- flames.py
+++ flames.py
@@ -1,16 +1,9 @@
+a = input("Enter the Number")
+print(f"Table of {a} is : ")
+try:
+    for i in range(1,11):
+        print(f"{int(a)} X {i} = {int(a)*i}")
 
-def greet(fx):
-  def mfx():
-        print("good morning")
-        fx()
-        print("thankyou for using our website")
-        return mfx()
-@greet
-def op():
-    print("hello world")
-op()
-
-
-
-
+except Exception as tryagain:
+    print(f"Sorry some error is there{tryagain} ")
 
