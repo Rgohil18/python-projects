5030f2c6a7112d7771f04049cde11359ac1fa10a
Rgohil18 <ravigohil2006@gmail.com>
2024-08-07 23:39:48 +0530

    class and super function practice

--- flames.py
+++ flames.py
@@ -1,9 +1,24 @@
-a = input("Enter the Number")
-print(f"Table of {a} is : ")
-try:
-    for i in range(1,11):
-        print(f"{int(a)} X {i} = {int(a)*i}")
+class basic:
+    def __init__(self,name,gender):
+        self.name = name
+        self.gender= gender
+        print(f"my name is {self.name}"
+              f" i am {gender}")
 
-except Exception as tryagain:
-    print(f"Sorry some error is there{tryagain} ")
+class details(basic):
+    def physical_measure(self,height,weight):
+        print(f"my height is {height},my weight is {weight}")
+
+class class_details(details):
+    def __init__(self,class1,div):
+        print(f"i am studying in class{class1}, division {div}")
+        super(basic).__init__()
+        super().physical_measure()
+
+
+x = basic("ravi","male")
+x = details()
+x.physical_measure("5'7","54")
+x = class_details("12th","h")
+
 
