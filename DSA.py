bf9f1e99ddcce414caf1008fdaa9e250741f33ff
Rgohil18 <ravigohil2006@gmail.com>
2024-08-22 22:43:48 +0530

    leet Max product of 2 num in array

--- DSA.py
+++ DSA.py
@@ -1,9 +1,8 @@
-def fn(list,target):
-    for i in list:
-        if (i==target):
-            print(f"{target} is in the list")
-        else:
-            print("The num is not in list")
-x = [1,2,3,4,5,68,5,7,4,]
-y = 68
-fn(x,y)
+def Max_product(array):
+    for i in array:
+        for y in array:
+            array = [i*y]
+            print("largest product is",max(array))
+
+list1 = [1,2,3,4,5]
+Max_product(list1)
