a749aee5ba81dd7635b31935798f1f950ff073b6
Rgohil18 <ravigohil2006@gmail.com>
2024-08-22 22:56:04 +0530

    leet middle fn

--- DSA.py
+++ DSA.py
@@ -1,8 +1,6 @@
-def Max_product(array):
-    for i in array:
-        for y in array:
-            array = [i*y]
-            print("largest product is",max(array))
+def middle(list):
+    y = list.remove([0,-1])
+    print(y)
 
-list1 = [1,2,3,4,5]
-Max_product(list1)
+z = [1,2,3,4,5]
+middle(z)
