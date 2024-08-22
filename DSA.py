052bce823b23eeb4cbea108eeb89261e283b860b
Rgohil18 <ravigohil2006@gmail.com>
2024-08-22 22:27:03 +0530

    leet 2 finding num in array

--- DSA.py
+++ DSA.py
@@ -1,9 +1,9 @@
-
-def two_sum(list,target):
+def fn(list,target):
     for i in list:
-        for o in list:
-            if (i+o==target):
-                print(i,o)
-x = [1,2,3,4,5]
-y = 5
-two_sum(x,y)
+        if (i==target):
+            print(f"{target} is in the list")
+        else:
+            print("The num is not in list")
+x = [1,2,3,4,5,68,5,7,4,]
+y = 68
+fn(x,y)
